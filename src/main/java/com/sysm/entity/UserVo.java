9a87e4c6c6d8562d315916ef9ad420149c3d3a17
yangxh <yangxh@sysware.com.cn>
2018-10-30 14:57:10 +0800

    20181030修改内容

---
+++ src/main/java/com/sysm/entity/UserVo.java
@@ -0,0 +1,29 @@
+package com.sysm.entity;
+
+/**
+ * @author : yangxh
+ * @version : 1.0
+ * @create 2018-10-30 14:55
+ * @Team : 系统集成部
+ * @description :
+ **/
+public class UserVo {
+    private Long id;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
